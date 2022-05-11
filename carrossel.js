36ce537df982075214b01b0f45484f52ead01b89
Daniel Froes <danielfroestbf@gmail.com>
2022-05-11 15:44:19 -0300

    feat: arrumando o carrossel

---
+++ carrossel.js
@@ -0,0 +1,9 @@
+new Glider(document.querySelector('.glider'), {
+    slidesToShow: 1,
+    draggable: true,
+    dots: '#dots',
+    arrows:{
+        prev: '.glider-prev',
+        next: '.glider-next'
+    }
+})
